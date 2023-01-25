bd6f22ff2c48be27a3b8207993f3f4f77e70a970
GitStudentSem <69849493+GitStudentSem@users.noreply.github.com>
2023-01-25 15:28:37 +0300

    float canvas with image & mouse event

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,6 +1,7 @@
-// npx parcel ./index.html
+// npx parcel ./src/index.html
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import nebula from "../img/nebula.jpeg";
 
 const renderer = new THREE.WebGLRenderer();
 
@@ -19,13 +20,45 @@ const camera = new THREE.PerspectiveCamera(
 
 const orbit = new OrbitControls(camera, renderer.domElement);
 
-camera.position.set(-90, 140, 140);
+camera.position.set(0, 0, 12);
 orbit.update();
 
-const ambientLight = new THREE.AmbientLight(0x333333);
-scene.add(ambientLight);
+const uniforms = {
+    u_time: { type: "f", value: 0.0 },
+    u_resolution: {
+        type: "v2",
+        value: new THREE.Vector2(
+            window.innerWidth,
+            window.innerHeight
+        ).multiplyScalar(window.devicePixelRatio),
+    },
+    u_mouse: {
+        type: "v2",
+        value: new THREE.Vector2(0.0, 0.0),
+    },
+    image: { type: "t", value: new THREE.TextureLoader().load(nebula) },
+};
 
+window.addEventListener("mousemove", function (e) {
+    uniforms.u_mouse.value.set(
+        e.screenX / window.innerWidth,
+        1 - e.screenY / window.innerHeight
+    );
+});
+
+const geometry = new THREE.PlaneGeometry(10, 10, 30, 30);
+const material = new THREE.ShaderMaterial({
+    vertexShader: document.getElementById("vertexShader").textContent,
+    fragmentShader: document.getElementById("fragmentShader").textContent,
+    wireframe: false,
+    uniforms,
+});
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+const clock = new THREE.Clock();
 function animate() {
+    uniforms.u_time.value = clock.getElapsedTime();
     renderer.render(scene, camera);
 }
 
