7f6545376c5989d7e6dc73bd72808a5f3570c95c
GitStudentSem <69849493+GitStudentSem@users.noreply.github.com>
2023-01-25 17:37:32 +0300

    add gitignore fix scripts package.json

--- src/js/box.js
+++
@@ -1,5 +0,0 @@
-import * as THREE from "three";
-
-const boxGeometry = new THREE.BoxGeometry();
-const boxMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-export const box = new THREE.Mesh(boxGeometry, boxMaterial);
