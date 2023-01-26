4e007b02562e0a8ef59e6394ee6fceb23b536b91
GitStudentSem <69849493+GitStudentSem@users.noreply.github.com>
2023-01-26 23:22:48 +0300

    plane and create object on plane

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,5 +1,6 @@
 // npx parcel ./index.html
 import * as THREE from "three";
+import { ObjectSpaceNormalMap } from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 const renderer = new THREE.WebGLRenderer();
@@ -19,10 +20,99 @@ const camera = new THREE.PerspectiveCamera(
 
 const orbit = new OrbitControls(camera, renderer.domElement);
 
-camera.position.set(0, 0, 0);
+camera.position.set(10, 15, -22);
 orbit.update();
 
-function animate() {
+// CREATE PLANE
+const planeMesh = new THREE.Mesh(
+    new THREE.PlaneGeometry(20, 20),
+    new THREE.MeshBasicMaterial({ side: THREE.DoubleSide, visible: false })
+);
+planeMesh.rotateX(-Math.PI / 2);
+scene.add(planeMesh);
+planeMesh.name = "ground";
+
+const grid = new THREE.GridHelper(20, 20);
+scene.add(grid);
+//--- CREATE PLANE
+
+// SQUARE
+const hightlightMesh = new THREE.Mesh(
+    new THREE.PlaneGeometry(1, 1),
+    new THREE.MeshBasicMaterial({ side: THREE.DoubleSide, transparent: true })
+);
+hightlightMesh.rotateX(-Math.PI / 2);
+hightlightMesh.position.set(0.5, 0, 0.5);
+scene.add(hightlightMesh);
+//---SQUARE
+
+const mousePosition = new THREE.Vector2();
+const raycaster = new THREE.Raycaster();
+let intersects;
+
+window.addEventListener("mousemove", (e) => {
+    mousePosition.x = (e.clientX / window.innerWidth) * 2 - 1;
+    mousePosition.y = -(e.clientY / window.innerHeight) * 2 + 1;
+    raycaster.setFromCamera(mousePosition, camera);
+    intersects = raycaster.intersectObjects(scene.children);
+    intersects.forEach((intersect) => {
+        if (intersect.object.name === "ground") {
+            const hightlightPos = new THREE.Vector3()
+                .copy(intersect.point)
+                .floor()
+                .addScalar(0.5);
+            hightlightMesh.position.set(hightlightPos.x, 0, hightlightPos.z);
+
+            const objectExist = addedObects.find((object) => {
+                return (
+                    object.position.x === hightlightMesh.position.x &&
+                    object.position.z === hightlightMesh.position.z
+                );
+            });
+
+            if (!objectExist) {
+                hightlightMesh.material.color.setHex(0xffffff);
+            } else {
+                hightlightMesh.material.color.setHex(0xff0000);
+            }
+        }
+    });
+});
+
+const sphereMesh = new THREE.Mesh(
+    new THREE.SphereGeometry(0.4, 4, 2),
+    new THREE.MeshBasicMaterial({ wireframe: true, color: 0xffea00 })
+);
+
+const addedObects = [];
+window.addEventListener("click", () => {
+    const objectExist = addedObects.find((object) => {
+        return (
+            object.position.x === hightlightMesh.position.x &&
+            object.position.z === hightlightMesh.position.z
+        );
+    });
+
+    if (!objectExist) {
+        intersects.forEach((intersect) => {
+            if (intersect.object.name === "ground") {
+                const sphereClone = sphereMesh.clone();
+                sphereClone.position.copy(hightlightMesh.position);
+                scene.add(sphereClone);
+                addedObects.push(sphereClone);
+                hightlightMesh.material.color.setHex(0xff0000);
+            }
+        });
+    }
+});
+
+function animate(time) {
+    hightlightMesh.material.opacity = 1 + Math.sin(time / 120);
+    addedObects.forEach((object) => {
+        object.rotation.x = time / 1000;
+        object.rotation.z = time / 1000;
+        object.position.y = 0.5 + 0.5 * Math.abs(Math.sin(time / 1000));
+    });
     renderer.render(scene, camera);
 }
 
