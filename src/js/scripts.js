322508d6a510aec6e1766715c4368867d71ecb05
GitStudentSem <69849493+GitStudentSem@users.noreply.github.com>
2023-01-26 21:05:57 +0300

    create dpropped sphere

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,10 +1,13 @@
 // npx parcel ./index.html
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import * as CANNON from "cannon-es";
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({ antialias: true });
 
 renderer.setSize(window.innerWidth, window.innerHeight);
+
+renderer.shadowMap.enabled = true;
 
 document.body.appendChild(renderer.domElement);
 
@@ -19,10 +22,111 @@ const camera = new THREE.PerspectiveCamera(
 
 const orbit = new OrbitControls(camera, renderer.domElement);
 
-camera.position.set(0, 0, 0);
+camera.position.set(0, 4, 10);
 orbit.update();
 
+/* LIGHT */
+const ambientLight = new THREE.AmbientLight(0x333333);
+scene.add(ambientLight);
+
+const directionLight = new THREE.DirectionalLight(0xffffff, 0.8);
+scene.add(directionLight);
+directionLight.position.set(0, 50, 0);
+directionLight.castShadow = true;
+directionLight.shadow.mapSize.width = 1024;
+directionLight.shadow.mapSize.height = 1024;
+/** LIGHT **/
+
+/* WORLD GENERATE */
+const world = new CANNON.World({ gravity: new CANNON.Vec3(0, -9.81, 0) });
+/**  WORLD GENERATE **/
+
+/* PLANE GENERATE */
+const planeGeo = new THREE.PlaneGeometry(10, 10);
+const planeMat = new THREE.MeshStandardMaterial({
+    color: 0xffffff,
+    side: THREE.DoubleSide,
+});
+const planeMesh = new THREE.Mesh(planeGeo, planeMat);
+scene.add(planeMesh);
+planeMesh.receiveShadow = true;
+
+const planePhysMat = new CANNON.Material();
+const planeBody = new CANNON.Body({
+    type: CANNON.Body.STATIC,
+    shape: new CANNON.Box(new CANNON.Vec3(5, 5, 0.001)),
+    material: planePhysMat,
+});
+planeBody.quaternion.setFromEuler(-Math.PI / 2, 0, 0);
+world.addBody(planeBody);
+/**  PLANE GENERATE **/
+
+const mouse = new THREE.Vector2();
+const intersectionPoint = new THREE.Vector3();
+const plainNormal = new THREE.Vector3();
+const plane = new THREE.Plane();
+const raycaster = new THREE.Raycaster();
+
+window.addEventListener("mousemove", function (e) {
+    mouse.x = (e.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = -(e.clientY / window.innerHeight) * 2 + 1;
+    plainNormal.copy(camera.position).normalize();
+    plane.setFromNormalAndCoplanarPoint(plainNormal, scene.position);
+    raycaster.setFromCamera(mouse, camera);
+    raycaster.ray.intersectPlane(plane, intersectionPoint);
+});
+
+/* SPHERE GENERATOR */
+let meshes = [];
+let bodies = [];
+window.addEventListener("click", function (e) {
+    const spherePhysMat = new CANNON.Material();
+    const sphereGeo = new THREE.SphereGeometry(0.125, 30, 30);
+    const sphereMat = new THREE.MeshStandardMaterial({
+        color: Math.random() * 0xffffff,
+        metalness: 0,
+        roughness: 0,
+    });
+    const sphereMesh = new THREE.Mesh(sphereGeo, sphereMat);
+    scene.add(sphereMesh);
+    sphereMesh.castShadow = true;
+
+    const sphereBody = new CANNON.Body({
+        mass: 1,
+        shape: new CANNON.Sphere(0.125),
+        position: new CANNON.Vec3(
+            intersectionPoint.x,
+            intersectionPoint.y,
+            intersectionPoint.z
+        ),
+        material: spherePhysMat,
+    });
+    world.addBody(sphereBody);
+
+    const planeSphereContactMat = new CANNON.ContactMaterial(
+        planePhysMat,
+        spherePhysMat,
+        { restitution: 0.9 }
+    );
+    world.addContactMaterial(planeSphereContactMat);
+
+    meshes.push(sphereMesh);
+    bodies.push(sphereBody);
+});
+/** SPHERE GENERATOR **/
+
+const timeStep = 1 / 60;
 function animate() {
+    world.step(timeStep);
+
+    planeMesh.position.copy(planeBody.position);
+    planeMesh.quaternion.copy(planeBody.quaternion);
+
+    for (let i = 0; i !== meshes.length; i++) {
+        meshes[i].position.copy(bodies[i].position);
+        meshes[i].quaternion.copy(bodies[i].quaternion);
+    }
+
     renderer.render(scene, camera);
 }
 
