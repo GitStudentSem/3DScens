2bb6d7200a6b9d9b1c8ec02a8d631be5cbb1d9fa
GitStudentSem <69849493+GitStudentSem@users.noreply.github.com>
2023-01-31 20:24:56 +0300

    add loading status bar

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -4,8 +4,8 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader.js";
 
 const hdrTextureURL = new URL(
-    "../img/MR_INT-003_Kitchen_Pierre.pic",
-    import.meta.url
+  "../img/MR_INT-003_Kitchen_Pierre.pic",
+  import.meta.url
 );
 
 const renderer = new THREE.WebGLRenderer();
@@ -17,10 +17,10 @@ document.body.appendChild(renderer.domElement);
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(
-    45,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
+  45,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
 );
 
 const orbit = new OrbitControls(camera, renderer.domElement);
@@ -32,42 +32,62 @@ renderer.outputEncoding = THREE.sRGBEncoding;
 renderer.toneMapping = THREE.ACESFilmicToneMapping;
 renderer.toneMappingExposure = 1.8;
 
-const loader = new RGBELoader();
+const loadingManager = new THREE.LoadingManager();
+
+const progressBar = document.getElementById("progress-bar");
+loadingManager.onStart = (url, item, total) => {
+  console.log("Started loading:", url);
+};
+
+loadingManager.onProgress = (url, loaded, total) => {
+  progressBar.value = (loaded / total) * 100;
+};
+
+const progressBarContainer = document.querySelector(".progress-bar-container");
+loadingManager.onLoad = () => {
+  progressBarContainer.style.display = "none";
+};
+
+loadingManager.onError = (url) => {
+  console.log("Error loading:", url);
+};
+
+const loader = new RGBELoader(loadingManager);
 loader.load(hdrTextureURL, (texture) => {
-    texture.mapping = THREE.EquirectangularReflectionMapping;
-    scene.background = texture;
-    scene.environment = texture;
+  texture.mapping = THREE.EquirectangularReflectionMapping;
+  scene.background = texture;
+  scene.environment = texture;
 
-    const sphere = new THREE.Mesh(
-        new THREE.SphereGeometry(1, 50, 50),
-        new THREE.MeshStandardMaterial({
-            roughness: 0,
-            metalness: 1,
-            color: 0xffea00,
-        })
-    );
-    scene.add(sphere);
-    sphere.position.x = 1.5;
+  const sphere = new THREE.Mesh(
+    new THREE.SphereGeometry(1, 50, 50),
+    new THREE.MeshStandardMaterial({
+      roughness: 0,
+      metalness: 1,
+      color: 0xffea00,
+    })
+  );
+  scene.add(sphere);
+  sphere.position.x = 1.5;
 
-    const sphere2 = new THREE.Mesh(
-        new THREE.SphereGeometry(1, 50, 50),
-        new THREE.MeshStandardMaterial({
-            roughness: 0,
-            metalness: 1,
-            color: 0x00ff00,
-        })
-    );
-    scene.add(sphere2);
-    sphere2.position.x = -1.5;
+  const sphere2 = new THREE.Mesh(
+    new THREE.SphereGeometry(1, 50, 50),
+    new THREE.MeshStandardMaterial({
+      roughness: 0,
+      metalness: 1,
+      color: 0x00ff00,
+    })
+  );
+  scene.add(sphere2);
+  sphere2.position.x = -1.5;
 });
 function animate() {
-    renderer.render(scene, camera);
+  renderer.render(scene, camera);
 }
 
 renderer.setAnimationLoop(animate);
 
 window.addEventListener("resize", function () {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
 });
